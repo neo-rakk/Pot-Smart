4f168e29245b32116419465f9aa344b3dda85c8b
Neorak <499298+neo-rakk@users.noreply.github.com>
2026-03-01 19:54:43 +0000

    v1

---
+++ .pio/build/esp32dev/lib871/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/lib871/WebServer/detail/mimetable.cpp.o: \
+ /home/codespace/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ /home/codespace/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ /home/codespace/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
